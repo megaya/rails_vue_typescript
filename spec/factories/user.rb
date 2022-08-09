e5fb5499848a35efab99016f4f02cac72d62687c
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-09 12:45:03 +0800

    Delete spec directory

--- spec/factories/user.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :user do
-    sequence(:name) { |n| "test_user_#{n}" }
-    sequence(:email) { |n| "test_user_#{n}@test.com" }
-    password { 'password' }
-  end
-end
